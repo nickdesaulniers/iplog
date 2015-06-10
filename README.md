e4a20129ff6189e5b5a3f1fe28eb99971b5054ae
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-06-10 15:19:56 -0700

    fix internal & external

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ iplog
 will print something like:
 
 ```
-Internal IP Addr: 127.0.0.1
-External IP Addr: 10.252.26.78
+Internal IP Addr: 10.252.26.78
+External IP Addr: 63.245.221.32
 ```
 
 Can replace `ipconfig getifaddr en0`
--- bin/ip.js
+++ bin/ip.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
 
+var http = require('http');
 var os = require('os');
 var colors = require('colors');
+
+http.request({
+  host: 'ifconfig.me',
+  path: '/ip',
+}, function (res) {
+  res.on('data', function (chunk) {
+    console.log('External IP Addr: ' + colors.yellow(chunk));
+  });
+}).end();
 
 var ifaces = os.networkInterfaces();
 
 Object.keys(ifaces).forEach(function (id) {
   var iface = ifaces[id];
   iface.forEach(function (conn) {
-    if (conn.family === 'IPv4') {
-      if (conn.internal) {
-        console.log('Internal IP Addr: ' + colors.green(conn.address));
-      } else {
-        console.log('External IP Addr: ' + colors.yellow(conn.address));
-      }
+    if (conn.family === 'IPv4' && !conn.internal) {
+      console.log('Internal IP Addr: ' + colors.green(conn.address));
     }
   });
 });
